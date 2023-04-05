cb13d3aa8a65138cf256c4c563469dd267d87688
scorpion00100 <danbetheliryivuze@gmail.com>
2023-04-05 17:46:08 +0200

    fixed again some bug

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,26 +1,37 @@
 #!/usr/bin/python3
 ''' Return information about his/her TODO list progress from an REST API '''
-import requests
-import sys
+from requests import get
 from sys import argv
 
-employee_id = sys.argv[1]
 
-url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
+def get_api():
+    ''' data from an API '''
+    emp_id = int(argv[1])
+    emp_name = ''
+    tasks_done = 0
+    tasks_total = 0
+    tasks_titles = []
 
-response = requests.get(url)
+    users_res = get('https://jsonplaceholder.typicode.com/users').json()
+    for user in users_res:
+        if user['id'] == emp_id:
+            emp_name = user['name']
+            break
 
-todos = response.json()
+    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
+    for task in tasks_res:
+        if task['userId'] == emp_id:
+            if task['completed']:
+                tasks_titles.append(task['title'])
+                tasks_done += 1
+            tasks_total += 1
 
-num_completed = 0
-for todo in todos:
-    if todo['completed']:
-        num_completed += 1
+    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
+                                                          tasks_done,
+                                                          tasks_total))
+    for title in tasks_titles:
+        print('\t {}'.format(title))
 
-print(f"Employee {todos[0]['userId']} is done with tasks ({num_completed}/{len(todos)}):")
-for todo in todos:
-    if todo['completed']:
-        print("\t" + todo['title'])
 
 if __name__ == '__main__':
     get_api()
